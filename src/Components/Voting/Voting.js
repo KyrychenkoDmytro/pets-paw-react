fc0cafb9f4c23a22beaacc6fdfdd235655c406a4
khardy <khardy-official@gmail.com>
2022-07-20 11:37:31 +0300

    and voting layout

--- src/Components/Voting/Voting.js
+++ src/Components/Voting/Voting.js
@@ -13,10 +13,38 @@ const Voting = () => {
             </nav>
             <div className='choice-wrapper'>
                 <div className='flex-wrapper-back'>
-                    <img className="btn-back" src="../images/voting/back.svg" alt="search" />
+                    <Link to="/"><img className="btn-back" src="../images/voting/back.svg" alt="search" /></Link>
                     <div className='voting-lable'>voting</div>
                 </div>
+                <div className="show-cat">
+                    <div><img src="./images/voting/like-white.svg" alt="like" /></div>
+                    <div><img src="./images/voting/favourites-white.svg" alt="favourites" /></div>
+                    <div><img src="./images/voting/dislike-white.svg" alt="dislike" /></div>
+                </div>
+                <div className="users-action">
+                    <div className="user">
+                        <div>22:35</div>
+                        <p>Image ID: <span>fQSunHvl8</span> was added to Favourites</p>
+                        <img src="./images/voting/favourites-small.svg" alt="favourites" />
+                    </div>
+                    <div className="user">
+                        <div>22:35</div>
+                        <p>Image ID: <span>HJd0XecNX</span> was added to Likes</p>
+                        <img src="./images/voting/like-small.svg" alt="like" />
+                    </div>
+                    <div className="user">
+                        <div>22:35</div>
+                        <p>Image ID: <span>BbMFS3bU</span> was added to Dislikes</p>
+                        <img src="./images/voting/dislike-small.svg" alt="dislike" />
+                    </div>
+                    <div className="user">
+                        <div>22:35</div>
+                        <p>Image ID: <span>fQSunHvl8</span> was added to Favourites</p>
+                        <img src="./images/voting/favourites-small.svg" alt="favourites" />
+                    </div>
+                </div>
             </div>
+
         </div>
     );
 }
