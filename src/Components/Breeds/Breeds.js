aae33d4a392b2c3f84d6bd104661f07f466d49a0
khardy <khardy-official@gmail.com>
2022-07-26 12:14:22 +0300

    add search limit

--- src/Components/Breeds/Breeds.js
+++ src/Components/Breeds/Breeds.js
@@ -2,44 +2,49 @@ import './Breeds.css';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
+const api_key = "DEMO_API_KEY";
+
 const breeds = ['All breeds', 'Abyssinian', 'Aegean', 'American Bobtail', 'American Curl', 'American Shorthair', 'American Wirehair', '...'];
-const limits = ['Limit: 5', 'Limit: 10', 'Limit: 15', 'Limit: 20']
+const limits = [{ name: 'Limit: 5', search: 'limit=5' }, { name: 'Limit: 10', search: 'limit=10' }, { name: 'Limit: 15', search: 'limit=15' }, { name: 'Limit: 20', search: 'limit=20' }];
+
 
 
 const Breeds = () => {
-
-    useEffect(() => {
-        fetch('https://api.thecatapi.com/v1/breeds')
-            .then((response) => response.json())
-            .then((data) => setallBreeds(data));
-    }, []);
-
-
-
     const navigate = useNavigate();
 
-    const [allBreeds, setallBreeds] = useState([]);
+    const [allBreeds, setAllBreeds] = useState([]);
     const [open, setOpen] = useState(false);
     const [openLimit, setOpenLimit] = useState(false);
     const [selected, setSelected] = useState(0);
-    const [selectedLimit, setSelectedLimit] = useState(0);
+    const [selectedLimit, setSelectedLimit] = useState({ name: 'Limit: 5', search: 'limit=5' });
+
+    useEffect(() => {
+        fetch(`https://api.thecatapi.com/v1/images/search?${selectedLimit.search}`, {
+            headers: {
+                'x-api-key': api_key
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => setAllBreeds(data));
+    }, [selectedLimit]);
 
     const selectLi = (i) => {
         setSelected(i);
         setOpen(false);
     }
 
-    const selectLimit = (i) => {
-        setSelectedLimit(i);
+    const selectLimit = (item) => {
+        setSelectedLimit(item);
         setOpenLimit(false);
     }
 
     const idBreed = (item) => {
         navigate(item.reference_image_id);
     }
-    let a = [...allBreeds];
-    a.length = 20;
-console.log(a);
+
+    console.log(allBreeds);
+    console.log(selectedLimit);
+
     return (
         <div className="Breeds">
             <input type="text" placeholder='Search for breeds by name' />
@@ -66,14 +71,14 @@ console.log(a);
                             </div>
                         )
                     }
-                    <button className='breeds-select select-limit'>
-                        <span onClick={() => setOpenLimit(!openLimit)}>{limits[selectedLimit]}</span>
+                    <button className='breeds-select select-limit' onClick={() => setOpenLimit(!openLimit)}>
+                        <span>{selectedLimit.name}</span>
                         <img src="./images/breeds/arrow-down.svg" alt="arrow" />
                     </button>
                     {openLimit && (
                         <div id="popup-limits">
                             <ul>
-                                {limits.map((item, i) => <li onClick={() => selectLimit(i)} key={item}>{item}</li>)}
+                                {limits.map((item) => <li onClick={() => selectLimit(item)} key={item.name}>{item.name}</li>)}
                             </ul>
                         </div>
                     )}
@@ -81,7 +86,7 @@ console.log(a);
                     <button className="breeds-sort"><img src="./images/breeds/a-z.svg" alt="a-z" /></button>
                 </div>
                 <div className="container-grid">
-                    {a.map((item, index) => <div onClick={() => idBreed(item)} key={item.image.id} style={{ background: `url(${item.image.url}) 0% 0% / cover` }} className={`item grid-${index + 1}`}></div>)}
+                    {allBreeds.map((item, index) => <div onClick={() => idBreed(item)} key={item.id} style={{ background: `url(${item.url}) 0% 0% / cover` }} className={`item grid-${index + 1}`}></div>)}
                 </div>
             </div>
         </div>
