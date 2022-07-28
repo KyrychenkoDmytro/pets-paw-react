b681ff167cbdb910712bc87c9cc5193d78fa3db7
khardy <khardy-official@gmail.com>
2022-07-28 22:07:14 +0300

    Gallery works

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,10 +1,53 @@
 import './Gallery.css';
+
 import { Link, NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+const api_key = "DEMO_API_KEY";
 
 const Gallery = () => {
-    return(
+    const [listBreeds, setListBreeds] = useState([{ name: 'none', id: '' }]);
+    const [breed, setBreed] = useState('');
+    const [allImages, setAllImages] = useState([]);
+    const [selectedLimit, setSelectedLimit] = useState('limit=5');
+    const [order, setOrder] = useState('order=RANDOM');
+    const [mimeTypes, setMimeTypes] = useState('');
+
+    const greedRowCount = selectedLimit.replace(/\D/g, '') * 0.6;
+
+    useEffect(() => {
+        fetch('https://api.thecatapi.com/v1/breeds', {
+            headers: {
+                'x-api-key': api_key
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                data = data.reduce((accum, item) => {
+                    accum.push({ name: `${item.name}`, id: `${item.id}`, key: `${item.reference_image_id}` });
+                    return accum;
+                }, [])
+                setListBreeds(data);
+            });
+    }, []);
+
+    useEffect(() => {
+        fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breed}&${selectedLimit}&${order}&${mimeTypes}`, {
+            headers: {
+                'x-api-key': api_key
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.length === 0) console.log('empty');
+                console.log(data);
+                setAllImages(data);
+            });
+    }, [selectedLimit, order, breed, mimeTypes]);
+
+    return (
         <div className="Gallery">
-<input type="text" placeholder='Search for breeds by name' />
+            <input type="text" placeholder='Search for breeds by name' />
             <div className="btn-search"><img src="../images/voting/search.svg" alt="search" /></div>
             <nav>
                 <NavLink to="/likes"><img src="./images/voting/like.svg" alt="like" /></NavLink>
@@ -19,49 +62,44 @@ const Gallery = () => {
                 </div>
                 <div className='select-wrapper'>
                     <label>
-                    <select name="breeds" id="breeds-select" className='breeds-select'>
-                        <option>Random</option>
-                        <option>Desc</option>
-                        <option>Asc</option>
-                    </select>
+                        <select name="breeds" id="breeds-select" className='breeds-select' onChange={(e) => setOrder(e.target.value)}>
+                            <option value="order=RANDOM">Random</option>
+                            <option value="order=DESC">Desc</option>
+                            <option value="order=ASC">Asc</option>
+                        </select>
                     </label>
-                    <select name="breeds" id="breeds-select" className='breeds-select'>
-                        <option>All</option>
-                        <option>Static</option>
-                        <option>Animated</option>
+                    <select name="breeds" id="breeds-select" className='breeds-select' onChange={(e) => setMimeTypes(e.target.value)}>
+                        <option value="">All</option>
+                        <option value="mime_types=jpg&mime_types=png">Static</option>
+                        <option value="mime_types=gif">Animated</option>
                     </select>
-                    <select name="breeds" id="breeds-select" className='breeds-select'>
-                        <option>None</option>
-                        <option>Abyssinian</option>
-                        <option>Bengal</option>
-                        <option>Aegean</option>
-                        <option>American Bobtail</option>
-                        <option>American Shorthair</option>
-                        <option>American Wirehair</option>
-                        <option>...</option>
+                    <select name="breeds" id="breeds-select" className='breeds-select' onChange={(e) => setBreed(e.target.value)}>
+                        <option value="">None</option>
+                        {listBreeds.map((item) => <option value={item.id} key={item.key + item.id}>{item.name}</option>)}
                     </select>
-                    <select name="breeds" id="breeds-select-limit" className='breeds-select select-limit'>
-                        <option>5 items per page</option>
-                        <option>10 items per page</option>
-                        <option>15 items per page</option>
-                        <option>20 items per page</option>
+                    <select name="breeds" id="breeds-select-limit" className='breeds-select select-limit' onChange={(e) => setSelectedLimit(e.target.value)}>
+                        <option value="limit=5">5 items per page</option>
+                        <option value="limit=10">10 items per page</option>
+                        <option value="limit=15">15 items per page</option>
+                        <option value="limit=20">20 items per page</option>
                     </select>
-                    <button className="gallery-update"><img src="./images/gallery/update.svg" alt="update" /></button>
+                    <button className="gallery-update" onClick={() => window.location.reload()}><img src="./images/gallery/update.svg" alt="update" /></button>
                 </div>
-                <div className="container-grid">
-                    <div className="item grid-1"></div>
-                    <div className="item grid-2"></div>
-                    <div className="item grid-3"></div>
-                    <div className="item grid-4"></div>
-                    <div className="item grid-5"></div>
-                    <div className="item grid-6"></div>
-                    <div className="item grid-7"></div>
-                    <div className="item grid-8"></div>
-                    <div className="item grid-9"></div>
+                <div className="container-grid" style={{ gridTemplateRows: `repeat(${greedRowCount}, 140px )` }}>
+                    {allImages.map((item, index) =>
+                        <div
+                            key={item.id}
+                            style={{ background: `url(${item.url}) 0% 0% / cover` }}
+                            className={`item grid-${index + 1}`}>
+                            <div className='grid-hover'>
+                                <button><img src="./images/voting/favourites-small.svg" alt="favourites" /></button>
+                            </div>
+                        </div>)}
                 </div>
             </div>
         </div>
     );
 }
+
 
 export default Gallery;
