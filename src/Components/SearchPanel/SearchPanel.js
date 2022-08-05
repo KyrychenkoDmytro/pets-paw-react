1d30be08c2b38e561fd784c10998a291382bce93
khardy <khardy-official@gmail.com>
2022-08-05 21:03:33 +0300

    SerchPanel add outline and clear value

--- src/Components/SearchPanel/SearchPanel.js
+++ src/Components/SearchPanel/SearchPanel.js
@@ -1,19 +1,28 @@
 import './SearchPanel.css';
 
 import { NavLink, Link } from 'react-router-dom';
-import { useRef } from 'react';
+import { useRef, useState} from 'react';
 import { useDispatch } from 'react-redux';
 
 import { searchValue } from '../../store/slices/search/searchPanelSlice';
 
 const SearchPanel = () => {
+    const [clearValue, setClearValue] = useState('');
     const valueRef = useRef();
     const dispatch = useDispatch();
 
+const handleSearch = () => {
+    let value = valueRef.current.value;
+    valueRef.current.focus();
+    dispatch(searchValue(value));
+    console.log(value);
+    setClearValue('');
+}
+
     return (
         <div className="SearchPanel">
-            <input type="text" placeholder='Search for breeds by name' ref={valueRef} />
-            <Link to="/search" onClick={() => dispatch(searchValue(valueRef.current.value))}><button className="btn-search"><img src="../images/voting/search.svg" alt="search" /></button></Link>
+            <input type="text" placeholder='Search for breeds by name' ref={valueRef} value={clearValue} onChange={(e)=>setClearValue(e.target.value)} />
+            <Link to="/search" onClick={handleSearch}><button className="btn-search"><img src="../images/voting/search.svg" alt="search" /></button></Link>
             <nav>
                 <NavLink to="/likes"><img src="/images/voting/like.svg" alt="like" /></NavLink>
                 <NavLink to="/favourites"><img src="/images/voting/favourites.svg" alt="favourites" /></NavLink>
